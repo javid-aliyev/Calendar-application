714f2cb0561604c7a63f799f2f27e2e50ad8854b
Javid Aliyev <lorderer12@gmail.com>
2020-03-23 15:19:25 +0400

    app.py modified

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ import datetime
 import sys
 import colorama
 
+# ! ============================ DECORATORS ===========
+def green_wrapper(fn):
+	def wrapper():
+		print(colorama.Fore.GREEN)
+		fn()
+		print(colorama.Style.RESET_ALL)
+	return wrapper
+
+def blue_wrapper(fn):
+	def wrapper():
+		print(colorama.Fore.BLUE)
+		fn()
+		print(colorama.Style.RESET_ALL)
+
+# ! ============================ SECONDARY FUNCTIONS (logic) ===========
 def __get_user_input(text):
 	"""Gets user input and catchs KeyboardInterrupt"""
 	try:
@@ -16,16 +31,11 @@ def __split_user_input(inp):
 	"""Splits user input"""
 	return inp.split() if len(inp.split()) != 0 else [""]
 
-def __print_invalid_input_error():
-	print(colorama.Fore.RED + "Invalid command" + colorama.Style.RESET_ALL)
-
 def __exec_command(inp):
 	"""Checks for any command calling"""
 	splitted = __split_user_input(inp)
 	if splitted[0] == "now":
-		print(datetime.datetime.now())
-	elif splitted[0] == "day":
-		print(datetime.datetime.today())
+		__print_now()
 	elif splitted[0] == "exit":
 		exit()
 	elif splitted[0] == "":
@@ -33,7 +43,15 @@ def __exec_command(inp):
 	else:
 		__print_invalid_input_error()
 
+# ! ============================ PRINT FUNCTIONS (commands) ===========
+def __print_invalid_input_error():
+	print(colorama.Fore.RED + "Invalid command" + colorama.Style.RESET_ALL)
 
+@green_wrapper
+def __print_now():
+	print(datetime.datetime.now())
+
+# ! ============================ MAIN FUNCTION (program entry point) ===========
 def main():
 	colorama.init()
 	while True:
