99c69ba7c81b9764535109c54f0ec15ac5bdc349
Javid Aliyev <lorderer12@gmail.com>
2020-03-23 15:41:28 +0400

    app.py modified | config.json added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import calendar
 import datetime
-import sys
+import os
 import colorama
 
 # ! ============================ DECORATORS ===========
@@ -16,6 +16,23 @@ def blue_wrapper(fn):
 		print(colorama.Fore.BLUE)
 		fn()
 		print(colorama.Style.RESET_ALL)
+	return wrapper
+
+def red_wrapper(fn):
+	def wrapper():
+		print(colorama.Fore.RED)
+		fn()
+		print(colorama.Style.RESET_ALL)
+	return wrapper
+
+# ! ============================ PRINT FUNCTIONS (commands) ===========
+@red_wrapper
+def __print_invalid_input_error():
+	print("Invalid command")
+
+@green_wrapper
+def __print_now():
+	print(datetime.datetime.now())
 
 # ! ============================ SECONDARY FUNCTIONS (logic) ===========
 def __get_user_input(text):
@@ -25,39 +42,29 @@ def __get_user_input(text):
 		return user_input
 	except KeyboardInterrupt:
 		print()
-		sys.exit()
-
-def __split_user_input(inp):
-	"""Splits user input"""
-	return inp.split() if len(inp.split()) != 0 else [""]
+		exit()
 
 def __exec_command(inp):
 	"""Checks for any command calling"""
-	splitted = __split_user_input(inp)
-	if splitted[0] == "now":
+	if inp == "now":
 		__print_now()
-	elif splitted[0] == "exit":
+	elif inp == "exit":
 		exit()
-	elif splitted[0] == "":
+	elif inp == "clear":
+		__clear_terminal()
+	elif inp == "":
 		pass
 	else:
 		__print_invalid_input_error()
 
-# ! ============================ PRINT FUNCTIONS (commands) ===========
-def __print_invalid_input_error():
-	print(colorama.Fore.RED + "Invalid command" + colorama.Style.RESET_ALL)
-
-@green_wrapper
-def __print_now():
-	print(datetime.datetime.now())
+def __clear_terminal():
+	os.system("clear")
 
 # ! ============================ MAIN FUNCTION (program entry point) ===========
 def main():
 	colorama.init()
 	while True:
 		user_input = __get_user_input("$ ")
-		
-		# commands
 		__exec_command(user_input)
 
 if __name__ == "__main__":
